66500f8291a69019a5916a31ec9445ea22a2a791
evinma <evinma@tencent.com>
2019-11-18 09:09:46 +0800

    feat: edit app.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          As myself.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
